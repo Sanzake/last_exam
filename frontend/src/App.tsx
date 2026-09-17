01dc4f7f81a1fbabd2026a469a7f1f7c15a96fc1
Daniel <danhab20@gmail.com>
2026-09-17 16:03:40 +0300

    Add app and index

---
+++ frontend/src/App.tsx
@@ -0,0 +1,27 @@
+import { Route, Routes } from "react-router";
+import "./App.css";
+import Protected from "./components/Protected/Protected";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+import UserPage from "./pages/UserPage";
+
+function App() {
+	return (
+		<Routes>
+			<Route path="/login" element={<LoginPage />} />
+			<Route path="/register" element={<RegisterPage />} />
+			<Route
+				path="/user"
+				element={
+					<Protected>
+						<UserPage />
+					</Protected>
+				}
+			/>
+
+			<Route path="*" element={<>404 - Not Found!</>} />
+		</Routes>
+	);
+}
+
+export default App;
